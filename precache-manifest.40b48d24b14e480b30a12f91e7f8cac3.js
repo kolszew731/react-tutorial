c79b21038b8251dc2abd6992f58e33beb5b8c366
Krzysztof Olszewski <kolszew73@gmail.com>
2019-01-06 22:31:49 +0100

    Updates

---
+++ precache-manifest.40b48d24b14e480b30a12f91e7f8cac3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "f697f447105415794c4c",
+    "url": "/react-tutorial/static/js/main.f697f447.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/react-tutorial/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "f697f447105415794c4c",
+    "url": "/react-tutorial/static/css/main.7bbb3d6c.chunk.css"
+  },
+  {
+    "revision": "5779c2b32ca435e7554d80493a3ba812",
+    "url": "/react-tutorial/index.html"
+  }
+];
